ab280b9bc6b329abecd083ae23e82f567f4d7879
Adam Pruner <adampruner99@gmail.com>
2017-11-26 00:01:04 -0800

    remove extra stuff

--- image_clean.py
+++ image_clean.py
@@ -4,27 +4,14 @@ import numpy as np
 import cv2
 from skimage.color import rgb2grey
 from pyspark.sql import SparkSession, functions, types, Row
+import os
+import glob
 
 spark = SparkSession.builder.appName('Weather Image Classifier').getOrCreate()
 
 assert sys.version_info >= (3, 4) # make sure we have Python 3.4+
 assert spark.version >= '2.2' # make sure we have Spark 2.2+
 
-import os
-import glob
-'''
-spark = SparkSession.builder.appName('weather classification').getOrCreate()
-
-assert sys.version_info >= (3, 4)  # make sure we have Python 3.4+
-assert spark.version >= '2.1'  # make sure we have Spark 2.1+
-
-# Placeholder schema based on exercise 11
-schema = types.StructType([
-    types.StructField('score', types.LongType(), False),
-    #types.StructField('score_hidden', types.BooleanType(), False),
-    types.StructField('subreddit', types.StringType(), False),
-])
-'''
 def path_to_time(path):
     timestamp = os.path.splitext(path)[0][-14:]
     #2017-05-01 16:00
