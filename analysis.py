dc9ee79cbed9d9eea4d6bfbbd4fe4cb139fe2809
Adam Pruner <adampruner99@gmail.com>
2017-11-26 01:18:46 -0800

    fix some dumb stuff

--- analysis.py
+++ analysis.py
@@ -27,16 +27,6 @@ assert sys.version_info >= (3, 4) # make sure we have Python 3.4+
 assert spark.version >= '2.2' # make sure we have Spark 2.2+
 
 def main():
-    schema_file = open('schema')
-    schema_lines = [i.strip() for i in schema_file.readlines()]
-    schema = types.StructType([types.StructField(i, types.StringType(), False) for i in schema_lines])
-    schema_file.close()
-    weather = spark.read.csv(sys.argv[3], schema=schema)  # .withColumn('filename', functions.input_file_name())
-
-    df = spark.read.json('cleaned-katkam')
-
-
-def main():
     df = spark.read.json('cleaned-katkam')
     schema_file = open('schema')
     schema_lines = [i.strip() for i in schema_file.readlines()]
@@ -49,8 +39,9 @@ def main():
     nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
     model = nb.fit(train)
 
-    df = df.toPandas()
-    g = 2
+
+
+    # g = 2
 
 if __name__=='__main__':
     main()
