ea19baf808e3cbbee4f1af4f75721cdfcaf6af50
hgdsraj <raj.axisos@gmail.com>
2017-11-25 21:27:01 -0800

    Fixed app name

--- weather-parse.py
+++ weather-parse.py
@@ -4,7 +4,7 @@ import glob
 import shutil
 from pyspark.sql import SparkSession, functions, types
 
-spark = SparkSession.builder.appName('reddit averages').getOrCreate()
+spark = SparkSession.builder.appName('Weather ETL').getOrCreate()
 
 assert sys.version_info >= (3, 4) # make sure we have Python 3.4+
 assert spark.version >= '2.1' # make sure we have Spark 2.1+
