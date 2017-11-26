ce26905f93fff3788f1cca1b78730f119cc8fcd7
Adam Pruner <adampruner99@gmail.com>
2017-11-26 01:02:58 -0800

    added schema stuff

--- analysis.py
+++ analysis.py
@@ -5,11 +5,13 @@ import cv2
 import shutil
 from skimage.color import rgb2grey
 from pyspark.sql import SparkSession, functions, types, Row
-from pyspark.sql.types import StructType, StructField, ArrayType, StringType, LongType
 import json
-schema = StructType([
-    StructField('Date/Time',StringType(),True),
-    StructField("image",ArrayType(LongType()),False)
+import os
+import glob
+
+schema = types.StructType([
+    types.StructField('Date/Time',types.StringType(),True),
+    types.StructField("image",types.ArrayType(types.LongType()),False)
 ])
 #https://stackoverflow.com/questions/31477598/how-to-create-an-empty-dataframe-with-a-specified-schema
 
@@ -23,24 +25,16 @@ spark = SparkSession.builder.appName('Weather Image Classifier').getOrCreate()
 assert sys.version_info >= (3, 4) # make sure we have Python 3.4+
 assert spark.version >= '2.2' # make sure we have Spark 2.2+
 
-import os
-import glob
-'''
-spark = SparkSession.builder.appName('weather classification').getOrCreate()
-
-assert sys.version_info >= (3, 4)  # make sure we have Python 3.4+
-assert spark.version >= '2.1'  # make sure we have Spark 2.1+
-
-# Placeholder schema based on exercise 11
-schema = types.StructType([
-    types.StructField('score', types.LongType(), False),
-    #types.StructField('score_hidden', types.BooleanType(), False),
-    types.StructField('subreddit', types.StringType(), False),
-])
-'''
-
 def main():
+    schema_file = open('schema')
+    schema_lines = [i.strip() for i in schema_file.readlines()]
+    schema = types.StructType([types.StructField(i, types.StringType(), False) for i in schema_lines])
+    schema_file.close()
+    weather = spark.read.csv(sys.argv[3], schema=schema)  # .withColumn('filename', functions.input_file_name())
+    
     df = spark.read.json('cleaned-katkam')
+
+
     df.show()
 
 if __name__=='__main__':
