8bfb213f1369cc484e0f15cdb8a739c91f1ed2b3
Adam Pruner <adampruner99@gmail.com>
2017-12-04 15:31:06 -0800

    alternate hopefully faster rgb method done

--- write_katkam_json.py
+++ write_katkam_json.py
@@ -33,7 +33,7 @@ def main():
             r[i // 3] = img[i]
             g[i // 3] = img[i + 1]
             b[i // 3] = img[i + 2]
-        rgb_img = np.append(r, g, b)
+        rgb_img = np.array([r, g, b]).flatten()
         with open('{}/{}'.format(out_directory, os.path.splitext(filename)[0][-21:]), 'w') as fp:
             json.dump({'time':path_to_time(filename), 'image': np.array_repr(rgb_img)}, fp)
         count -= 1
