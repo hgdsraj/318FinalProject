28e92a8d5fb7f8cb8f2a3d87e7337806fb276392
Adam Pruner <adampruner99@gmail.com>
2017-12-05 22:01:30 -0800

    change some variable names

--- weather_parse.py
+++ weather_parse.py
@@ -9,16 +9,16 @@ assert spark.version >= '2.1' # make sure we have Spark 2.1+
 
 in_directory = sys.argv[1] # should be yvr-weather
 out_directory = sys.argv[2] # cleaned-weather
-tmpdir = sys.argv[3] # temporary directory for intermediate results
+tempdir = sys.argv[3] # temporary directory for intermediate results, should be tempdir
 
-def main(in_directory, out_path, tmpdir):
-    directory = tmpdir
+def main(in_directory, out_path, tempdir):
+    tmpdir = tempdir
     schema_file = open('schema')
     schema_lines = [i.strip() for i in schema_file.readlines()]
     schema = types.StructType([types.StructField(i, types.StringType(), False) for i in schema_lines])
     schema_file.close()
 
-    weather = spark.read.csv(directory, schema=schema)#.withColumn('filename', functions.input_file_name())
+    weather = spark.read.csv(tmpdir, schema=schema)#.withColumn('filename', functions.input_file_name())
     weather = weather[weather['Weather'] != 'NA']
     weather_columns = [i for i in weather.schema.names if 'Flag' not in i]
     weather = weather.select(
@@ -31,7 +31,7 @@ def main(in_directory, out_path, tmpdir):
 
     weather.write.csv(out_path, mode='overwrite')
 
-    shutil.rmtree(directory) #remove tempdir
+    shutil.rmtree(tmpdir) #remove tempdir
 
 if __name__=='__main__':
-    main(in_directory, out_directory, tmpdir)
+    main(in_directory, out_directory, tempdir)
