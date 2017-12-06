46d6759f8b1937505343a3b9f5977a8fe8270441
Adam Pruner <adampruner99@gmail.com>
2017-12-05 21:58:58 -0800

    more minor changes for code style

--- weather_parse.py
+++ weather_parse.py
@@ -7,13 +7,17 @@ spark = SparkSession.builder.appName('Weather ETL - Clean Weather Data').getOrCr
 assert sys.version_info >= (3, 4) # make sure we have Python 3.4+
 assert spark.version >= '2.1' # make sure we have Spark 2.1+
 
+in_directory = sys.argv[1] # should be yvr-weather
+out_directory = sys.argv[2] # cleaned-weather
+tmpdir = sys.argv[3] # temporary directory for intermediate results
+
 def main(in_directory, out_path, tmpdir):
     directory = tmpdir
     schema_file = open('schema')
     schema_lines = [i.strip() for i in schema_file.readlines()]
     schema = types.StructType([types.StructField(i, types.StringType(), False) for i in schema_lines])
     schema_file.close()
-    
+
     weather = spark.read.csv(directory, schema=schema)#.withColumn('filename', functions.input_file_name())
     weather = weather[weather['Weather'] != 'NA']
     weather_columns = [i for i in weather.schema.names if 'Flag' not in i]
@@ -30,7 +34,4 @@ def main(in_directory, out_path, tmpdir):
     shutil.rmtree(directory) #remove tempdir
 
 if __name__=='__main__':
-    in_directory = sys.argv[1] # should be yvr-weather
-    out_path = sys.argv[2] # cleaned-weather
-    tmpdir = sys.argv[3]
-    main(in_directory, out_path, tmpdir)
+    main(in_directory, out_directory, tmpdir)
